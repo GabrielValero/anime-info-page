74f7c06c4756a7a0de2ecaec06098b36fd788021
Gabriel Valero <ninjagabo131280@gmail.com>
2020-08-26 23:41:56 -0400

    asd

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Navbar from '../Components/Navbar';
-import Head from 'next/Head';
+import Head from 'next/head';
 export default function Home() {
   return (
   	<div style={{minHeight:"100vh"}}>
+  		<Head>
+  			<title>Index</title>
+  		</Head>
   		<Navbar/>
 
   		<h1>Hola Mundo XD</h1>
