b15cdac7d484f52642019c69d104e64ef260eca4
Gabriel Valero <ninjagabo131280@gmail.com>
2020-08-27 03:25:54 -0400

    Añadidura de las opciones de anime y manga, arreglos responsive de ellas y de la barra de navegacion

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -4,13 +4,14 @@ import Link from 'next/link';
 
 export default function Navbar(){
 	return (
-		<nav className="navbar navbar-expand-md">
+		<nav className="navbar navbar-expand-sm">
 			<Head>
 			<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700;900&display=swap" rel="stylesheet"/>
 				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"/>
 				<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 				<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
 				<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 			</Head>
 		
 			<div className="button">
--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,82 @@ export default function Home() {
   			<title>Index</title>
   		</Head>
   		<Navbar/>
+  		<header className="header">
+  			<div className="Options col-xl-10">
+  				<div className="option">
+	  				<img src="/images/SoloLevelin.png" alt="SoloLevelin Img" />
+	  				<h2>MANGA</h2>
+	  			</div>
+	  			<div className="option">
+	  				<img src="/images/KimetsuNoYaiba.png" alt="KimetsuNoYaiba Img" />
+	  				<h2>ANIME</h2>
+	  			</div>
+	  			</div>
+  		</header>
+  		<style jsx>{`
+  			header{
+  				position: relative;
+  				height: 90vh;
+  				display: flex;
+  				justify-content: center;
+  				background-size: cover;
+  				background-position: bottom; 
+  				background-image: url('/images/Group 21.png');
+  			}
+  			.Options{
+  				position: absolute;
+  				bottom: 0;
+  				display: flex;
+  				flex-wrap:wrap;
+  				justify-content: space-around;
+  				transform: translateY(50%);
+  			}
 
-  		<h1>Hola Mundo XD</h1>
+  			.option{
+  				display: flex;
+  				align-items: center;
+  				justify-content: center;
+  				position: relative;
+  			}
+  			.option img{
+  				width: 400px;
+  				margin: 30px;
+  				border-radius: 15px;
+  			}
+  			.option h2{
+  				position: absolute;
+  				color: white;
+  				font-weight: 700;
+  				padding:0;
+  				margin:0;
+  				z-index: 2;
+  			}
+  			@media(max-width:950px){
+  				.option img{
+  					width: 60%; 
+  				}
+  			}
+  			@media(max-width:650px){
+  				.option{
+  					margin: 10px;
+  				}
+  				.option img{
+  					width: 70%;
+  					margin: 10px;
+  				}
+  			}
+  			@media(max-width:450px){
+  				.option{
+  					margin: 10px;
+  				}
+  				.option img{
+  					width: 80%;
+  				}
+  				header{
+  					height: 70vh;
+  				}
+  			}
+  		`}</style>
   	</div>
-
   )
 }
